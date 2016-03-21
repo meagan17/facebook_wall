7f84e782704c8741c2238656f0c6cf1d21334017
meagan17 <mramo4555@withs.org>
2016-03-21 13:49:31 +0000

    Facebook wall project

--- js/wall.js
+++ js/wall.js
@@ -2,5 +2,13 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
+	$("submit").click(function(){
+		
+	});
+	
+	$('#post-btn').click(function(){
+		var nameText = $("#message").val();
+		$("#wall").append("<div>" + "<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + nameText + "</div>");
+	});
 	// YOUR CODE GOES HERE
 });
